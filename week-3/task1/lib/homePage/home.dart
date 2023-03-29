7dfec568586208399c1c63a45969947095bedce4
Zain Ali <zaynmr810@gmail.com>
2023-03-29 22:39:58 +0500

    Flutter Basic Widgets

---
+++ week-3/task1/lib/homePage/home.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.cyan,
+        title: const Center(child: Text("Flutter",style: TextStyle(fontSize: 28),)),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            
+          ],
+        ),
+      ),
+    );
+  }
+}
