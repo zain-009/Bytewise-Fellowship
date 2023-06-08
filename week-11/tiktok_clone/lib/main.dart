315c3f405519a19949ae44c90159a9f80d4d5886
Zain Ali <zaynmr810@gmail.com>
2023-06-08 14:06:29 +0500

    TikTok Clone

--- week-11/tiktok_clone/lib/main.dart
+++ week-11/tiktok_clone/lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:get/get.dart';
 import 'package:tiktok_clone/constants.dart';
+import 'package:tiktok_clone/controllers/auth_controller.dart';
 import 'package:tiktok_clone/views/screens/auth/signup_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp().then((value) {
+    Get.put(AuthController());
+  } );
   runApp(const MyApp());
 }
 
